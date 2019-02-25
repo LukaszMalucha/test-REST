4506f5a97e99d1fa982ed4a44161283752051d1f
Lukasz_Malucha <lucasmalucha@gmail.com>
2019-02-25 11:34:42 +0000

    test

--- core/models.py
+++ core/models.py
@@ -1,39 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseModel, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 
-class UserManager(BaseUserManager):
     
-    def create_user(self, email, password=None, **extra_fields):
-        
-        if not email:
-            raise ValueError('no email')
-            
-        user = self.model(email=self.normalize_email(email), **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        
-        return user
-        
-    def create_superuser(self, email, password):
-        
-        user = self.create_user(email, password)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        
-        return user
-        
-class User(AbstractBaseModel, PermissionsMixin):
     
-    email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
     
-    objects = UserManager()
     
-    USERNAME_FIELD = 'email'
     
-        
-        
+    
+    
+    
+    
+    
+    
+    
